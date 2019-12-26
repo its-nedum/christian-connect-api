714d75ac84a64097b0d69c69e010a05436c2ad5c
its-nedum <de.magnificent1@gmail.com>
2019-12-26 11:19:11 +0100

    feature(implemented admin post video)

--- routes/admins.js
+++ routes/admins.js
@@ -30,41 +30,114 @@ router.post('/music',  async (req, res) => {
     const image = req.files.image
     const music = req.files.music
 
-    let urls = [];
+    //Check if song already exist
+    await client.query("SELECT * FROM music WHERE music_title = $1", [musicTitle], async (err, result) => {
+        if(err) { console.log(err) }
+        //If song already exist do this
+        if(result.rows[0]){
+            return res.status(400).json({
+                message: 'Song already exist'
+            })
+        } else {
+            //If song does not exist in database then we add it
+            let urls = [];
 
-    //Save the media files in an array
-    let media = [image.tempFilePath, music.tempFilePath];
-    for(let singleTemp of media){
-      await  cloudinary.uploader.upload(singleTemp, async (err, result) => {
-            if(err){
-                console.log(err)
+            //Save the media files in an array
+            let media = [image.tempFilePath, music.tempFilePath];
+            for(let singleTemp of media){
+              await  cloudinary.uploader.upload(singleTemp, async (err, result) => {
+                    if(err){
+                        console.log(err)
+                    }
+                   await urls.push(result.url)
+                })
+              
             }
-           await urls.push(result.url)
-        })
-      
-    }
-    //Save data to Database
-    await client.query("INSERT INTO music(music_title, music_about, image_url, music_url, category, uploaded_by, created_at)VALUES($1, $2, $3, $4, $5, $6, current_timestamp)",
-                    [musicTitle, musicAbout, urls[0], urls[1], category, uploadedBy], (err) => {
-                     if(err){console.log(err)}  
-                    
-                     res.status(201).json({
-                        status: 'success',
-                        message: 'Song added successfully',
-                        data: {
-                            musicTitle,
-                            musicAbout,
-                            imageUrl: urls[0],
-                            musicUrl: urls[1],
-                            category,
-                            uploadedBy,
-                            created_at: Date.now()
-                        }
-                    })
-
-                    })
-     
+            //Save data to Database
+            await client.query("INSERT INTO music(music_title, music_about, image_url, music_url, category, uploaded_by, created_at)VALUES($1, $2, $3, $4, $5, $6, current_timestamp)",
+                            [musicTitle, musicAbout, urls[0], urls[1], category, uploadedBy], (err) => {
+                             if(err){console.log(err)}  
+                            
+                             res.status(201).json({
+                                status: 'success',
+                                message: 'Song added successfully',
+                                data: {
+                                    musicTitle,
+                                    musicAbout,
+                                    imageUrl: urls[0],
+                                    musicUrl: urls[1],
+                                    category,
+                                    uploadedBy,
+                                    created_at: Date.now()
+                                }
+                            })
+        
+                            })
+        }
     })
+
+})
+
+//POST A VIDEO TO DATABASE
+router.post('/video', async (req, res) => {
+    const videoTitle = req.body.videoTitle;
+    const videoAbout = req.body.videoAbout;
+    const image = req.files.image;
+    const video = req.files.video;
+    const category = 'Video';
+    const uploadedBy = 'Chinedu Emesue' //await getAdminName(req);
+
+    //Check if song already exist
+    await client.query("SELECT * FROM video WHERE video_title = $1", [videoTitle], async (err, result) => {
+        if(err) { console.log(err) }
+        //If song already exist do this
+        if(result.rows[0]){
+            return res.status(400).json({
+                message: 'Video already exist'
+            })
+        } else {
+            //If song does not exist in database then we add it
+            let urls = [];
+
+            //Save the media files in an array
+            let media = [image.tempFilePath, video.tempFilePath];
+            for(let singleTemp of media){
+              await  cloudinary.uploader.upload(singleTemp, async (err, result) => {
+                    if(err){
+                        console.log(err)
+                    }
+                   await urls.push(result.url)
+                })
+              
+            }
+            //Save data to Database
+            await client.query("INSERT INTO video(video_title, video_about, image_url, video_url, category, uploaded_by, created_at)VALUES($1, $2, $3, $4, $5, $6, current_timestamp)",
+                            [videoTitle, videoAbout, urls[0], urls[1], category, uploadedBy], (err) => {
+                             if(err){console.log(err)}  
+                            
+                             res.status(201).json({
+                                status: 'success',
+                                message: 'Video added successfully',
+                                data: {
+                                    videoTitle,
+                                    videoAbout,
+                                    imageUrl: urls[0],
+                                    videoUrl: urls[1],
+                                    category,
+                                    uploadedBy,
+                                    created_at: Date.now()
+                                }
+                            })
+        
+                            })
+        }
+    })
+
+})
+
+
+
+
 
 
 module.exports = router;
