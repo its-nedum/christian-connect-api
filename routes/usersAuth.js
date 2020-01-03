66e755cfe355f8ace39afe4b0f5ad0c5031ba366
its-nedum <de.magnificent1@gmail.com>
2020-01-03 20:59:28 +0100

    feature(implemented profile update and get user profile info)

--- routes/usersAuth.js
+++ routes/usersAuth.js
@@ -2,16 +2,30 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fileUpload = require('express-fileupload')
 const Op = require('sequelize').Op;
 
 //IMPORT DATABASE AND MODEL
 const db = require('../database/sequelizeConnect')
 const Users = require('../models/Users')
 
+//SETUP CLOUDINARY
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+//SETUP FILEUPLOAD
+router.use(fileUpload({
+    useTempFiles: true
+}))
+
 //test sequelize
-// db.authenticate()
-//     .then( () => console.log('Sequelize DB connected'))
-//     .catch( (err) => console.log('Error: ' + err))
+db.authenticate()
+    .then( () => console.log('Database connected successfully'))
+    .catch( (err) => console.log('Error: ' + err))
 
 //USER REGISTER
 router.post('/signup', async (req, res) => {
@@ -58,7 +72,6 @@ router.post('/signup', async (req, res) => {
                                 birthdate
                             }).then( (user) => {
                                 const name = firstname + ' ' + lastname;
-                                
                                 const token = jwt.sign({name, email, username}, process.env.SECRET_TOKEN, {expiresIn: '7d'})
                                 res.status(201).json({
                                     status: 'User created successfully',
@@ -143,11 +156,67 @@ router.post('/signin', async (req, res) => {
 
 })
 
-//USER UPDATE ACCOUNT
+//USER UPDATE ACCOUNT (NOTE: User can not update their email or username)
+router.post('/user/:userId/profile', async (req, res) => {
+    let {firstname, lastname, telephone, state, gender, 
+         birthdate, country, city, relationship_status,
+         work, school, about_me } = req.body;
+    const id = req.params.userId;
+    
+    Users.update(
+        {
+            firstname,
+            lastname,
+            telephone,
+            state,
+            gender,
+            birthdate,
+            country,
+            city,
+            relationship_status,
+            work,
+            school,
+            about_me
+        },
+        { where: { id } }
+    ).then( (user) => {
+        res.status(201).json({
+            status: 'Profile updated successfully',
+            data: user
+        })
+    }).catch( (err) => {
+        res.status(500).json({
+            error: "Something went wrong, please try again later"
+        })
+    })
+})
 
+//USER UPLAOD PROFILE PICTURE
+router.post('/avatar', async (req, res) => {
+    let avatar = req.files.avatar;
+})
+
+//USER UPDATE PROFILE PICTURE
 
 //USER UPDATE PASSWORD
 
 //GET USER PROFILE
+router.get('/user/:userId', async (req, res) => {
+    const id = req.params.userId;
+    //Find and return the user account with id = userId
+    Users.findOne({
+        where: { id }
+    }).then( (user) => {
+        res.status(200).json({
+            data: user
+        })
+    }).catch( (err) => {
+        res.status(500).json({
+            error: "Something went wrong, please try again later"
+        })
+    })
+
+})
+
 
 module.exports = router
