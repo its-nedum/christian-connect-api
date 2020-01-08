e2872d5b40efe806858b09bc67d42dbbbc1e450d
its-nedum <de.magnificent1@gmail.com>
2020-01-08 11:36:07 +0100

    chores(modified api doc route)

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 //API Documentation ROUTE
-app.use('api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // ADMINS ROUTE
 const adminRouter = require('./routes/admins')
