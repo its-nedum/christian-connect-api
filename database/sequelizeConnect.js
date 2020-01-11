ce005a37dd88a88ec16396f4736fbc774cbfd20d
its-nedum <de.magnificent1@gmail.com>
2020-01-11 17:41:40 +0100

    chores(change db url)

--- database/sequelizeConnect.js
+++ database/sequelizeConnect.js
@@ -1,7 +1,7 @@
 const sequelize = require('sequelize');
 
-//const conString = process.env.ELEPHANT_SQL_DB
-const conString = process.env.LOCAL_DB_URL
+const conString = process.env.ELEPHANT_SQL_DB
+//const conString = process.env.LOCAL_DB_URL
 
 const db = new sequelize(conString)
 
