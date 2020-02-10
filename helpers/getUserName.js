6fb602acb4a5bc9cee37d1022ea9dc2b65eb0e94
its-nedum <de.magnificent1@gmail.com>
2020-02-10 12:52:12 +0100

    bug(fixed the error with getUserName for comments)

--- helpers/getUserName.js
+++ helpers/getUserName.js
@@ -2,22 +2,31 @@ const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res) => {
     let authToken = req.headers.authorization;
-    if(typeof authToken !== undefined || req.headers.authorization.split(' ')[1] !== null){
-    let username = [];
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, process.env.SECRET_TOKEN, (err, decodedToken) => {
-        if(err){
-            let { message } = err;
-            if(message == 'jwt expired'){
-              return res.status(401).json({
-                    message: 'Token expired'
-                })
-            }
-        }
-        username.push(decodedToken.name)
-    })
-    return username[0]
-    }else{
-        return 'Anonymous';
+    let token = authToken.split(' ')[1];
+    
+    switch (token) {
+        case 'null':
+                return 'Anonymous';
+            break;
+        case 'undefined':
+                return 'Anonymous';
+            break;
+        default:
+            let username = [];
+            jwt.verify(token, process.env.SECRET_TOKEN, (err, decodedToken) => {
+                if(err){
+                    let { message } = err;
+                    if(message == 'jwt expired'){
+                      return res.status(401).json({
+                            message: 'Token expired'
+                        })
+                    }
+                }
+                username.push(decodedToken.name)
+            })
+            return username[0]
+            break;
     }
+
+    
 }
