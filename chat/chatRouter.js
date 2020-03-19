5a76e471e90113e4175b6314ccd4046027d55fe5
its-nedum <de.magnificent1@gmail.com>
2020-03-19 13:14:03 +0100

    features(chat system server updated)

--- chat/chatRouter.js
+++ chat/chatRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/chat', (req, res) => {
+router.get('/chat-room', (req, res) => {
     res.send('We are up and running')
 })
 
--- server.js
+++ server.js
@@ -60,16 +60,22 @@ io.on('connection', (socket) => {
 
     if(error){ return callback(error)}
     socket.emit('message', {user: 'admin', text: `${user.username}, welcome to the room ${user.room}`})
-    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined`})
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.username}, has joined`})
 
     socket.join(user.room)
+
+    //Get total user in room
+    io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
 
     callback()
   })
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id)
-    io.to(user.room).emit('message', {user: user.name, text: message})
+    io.to(user.room).emit('message', {user: user.username, text: message})
+
+    //user in room
+    io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user, room)})
 
     callback()
   })
@@ -77,7 +83,7 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id)
     if(user){
-      io.to(user.room).emit('message', {user: 'admin', test: `${user.name} has left.`})
+      io.to(user.room).emit('message', {user: 'admin', text: `${user.username} has left.`})
     }
   })
 
